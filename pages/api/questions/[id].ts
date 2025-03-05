62fc3da8abd944b1e892e4dd051999be2375f98b
douglas <douglimaonline@gmail.com>
2025-03-05 20:30:36 -0300

    Implement api Questionnaire

--- pages/api/questions/[id].ts
+++ pages/api/questions/[id].ts
@@ -4,7 +4,11 @@ import { Question } from '@/pages/model/question'
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Question[]>
+  res: NextApiResponse<Question | string>
 ) {
-  res.status(200).json(questions.map((q) => q.toObject()))
+  const id = Number(req.query.id)
+  const question = questions.find((q) => q.id === id)?.toObject()
+  return question
+    ? res.status(200).json(question)
+    : res.status(204).send('204 No Content')
 }
