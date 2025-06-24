01dc40148ec3c355e78714d465ec7f49790d636d
douglas <douglimaonline@gmail.com>
2025-06-23 22:02:00 -0300

    Update questions

--- pages/api/questionsBank.ts
+++ pages/api/questionsBank.ts
@@ -2,11 +2,107 @@ import QuestionModel from '../../model/question'
 import AnswerModel from '../../model/answer'
 
 const questions: QuestionModel[] = [
-  new QuestionModel(301, 'Qual é a sexta nota da escala musical?', [
-    AnswerModel.wrongAnswer('Mi'),
-    AnswerModel.wrongAnswer('Ré'),
-    AnswerModel.wrongAnswer('Sol'),
-    AnswerModel.rightAnswer('Lá'),
+  new QuestionModel(301, 'Qual é a capital do estado da Bahia?', [
+    AnswerModel.wrongAnswer('Recife'),
+    AnswerModel.wrongAnswer('Fortaleza'),
+    AnswerModel.wrongAnswer('Maceió'),
+    AnswerModel.rightAnswer('Salvador'),
+  ]),
+  new QuestionModel(
+    302,
+    'Qual é o número da emergência da Polícia Militar no Brasil?',
+    [
+      AnswerModel.wrongAnswer('1900'),
+      AnswerModel.wrongAnswer('192'),
+      AnswerModel.wrongAnswer('193'),
+      AnswerModel.rightAnswer('190'),
+    ]
+  ),
+  new QuestionModel(
+    303,
+    'Qual é a sigla do imposto que incide sobre produtos industrializados no Brasil?',
+    [
+      AnswerModel.wrongAnswer('IPVA'),
+      AnswerModel.wrongAnswer('ICMS'),
+      AnswerModel.wrongAnswer('ISS'),
+      AnswerModel.rightAnswer('IPI'),
+    ]
+  ),
+  new QuestionModel(
+    304,
+    'Qual é o rio mais extenso que passa pelo território brasileiro?',
+    [
+      AnswerModel.wrongAnswer('Rio São Francisco'),
+      AnswerModel.wrongAnswer('Rio Paraná'),
+      AnswerModel.wrongAnswer('Rio Tocantins'),
+      AnswerModel.rightAnswer('Rio Amazonas'),
+    ]
+  ),
+  new QuestionModel(301, 'Qual é o maior planeta do sistema solar?', [
+    AnswerModel.wrongAnswer('Terra'),
+    AnswerModel.wrongAnswer('Marte'),
+    AnswerModel.wrongAnswer('Vênus'),
+    AnswerModel.rightAnswer('Júpiter'),
+  ]),
+  new QuestionModel(306, 'Qual é o banco estatal mais antigo do Brasil?', [
+    AnswerModel.wrongAnswer('Caixa Econômica Federal'),
+    AnswerModel.wrongAnswer('Banco Central'),
+    AnswerModel.wrongAnswer('Banco Mercantil'),
+    AnswerModel.rightAnswer('Banco do Brasil'),
+  ]),
+  new QuestionModel(
+    307,
+    'Em qual cidade brasileira está localizado o Cristo Redentor?',
+    [
+      AnswerModel.wrongAnswer('Salvador'),
+      AnswerModel.wrongAnswer('Belo Horizonte'),
+      AnswerModel.wrongAnswer('Brasília'),
+      AnswerModel.rightAnswer('Rio de Janeiro'),
+    ]
+  ),
+  new QuestionModel(315, 'Quem foi o primeiro homem a pisar na Lua?', [
+    AnswerModel.wrongAnswer('Buzz Aldrin'),
+    AnswerModel.wrongAnswer('Yuri Gagarin'),
+    AnswerModel.wrongAnswer('Michael Collins'),
+    AnswerModel.rightAnswer('Neil Armstrong'),
+  ]),
+  new QuestionModel(309, 'O que significa o SUS?', [
+    AnswerModel.wrongAnswer('Serviço Único da Saúde'),
+    AnswerModel.wrongAnswer('Sistema Urbano de Saúde'),
+    AnswerModel.wrongAnswer('Saúde Unificada do Sistema'),
+    AnswerModel.rightAnswer('Sistema Único de Saúde'),
+  ]),
+  new QuestionModel(
+    310,
+    'Qual é o nome do exame nacional utilizado para ingressar nas universidades públicas?',
+    [
+      AnswerModel.wrongAnswer('PAS'),
+      AnswerModel.wrongAnswer('ENAD'),
+      AnswerModel.wrongAnswer('OBMEP'),
+      AnswerModel.rightAnswer('ENEM'),
+    ]
+  ),
+  new QuestionModel(
+    311,
+    'Qual desses pratos é típico da culinária nordestina?',
+    [
+      AnswerModel.wrongAnswer('Feijoada'),
+      AnswerModel.wrongAnswer('Churrasco gaúcho'),
+      AnswerModel.wrongAnswer('Pão de queijo'),
+      AnswerModel.rightAnswer('Baião de dois'),
+    ]
+  ),
+  new QuestionModel(310, 'Quem pintou a obra "A Última Ceia"?', [
+    AnswerModel.wrongAnswer('Michelangelo'),
+    AnswerModel.wrongAnswer('Rafael'),
+    AnswerModel.wrongAnswer('Van Gogh'),
+    AnswerModel.rightAnswer('Leonardo da Vinci'),
+  ]),
+  new QuestionModel(313, 'Quem foi Ayrton Senna?', [
+    AnswerModel.wrongAnswer('Cantor'),
+    AnswerModel.wrongAnswer('Político'),
+    AnswerModel.wrongAnswer('Escritor'),
+    AnswerModel.rightAnswer('Piloto de Fórmula 1'),
   ]),
   new QuestionModel(302, 'Qual é a nota que vem depois de Sol?', [
     AnswerModel.wrongAnswer('Si'),
@@ -14,103 +110,11 @@ const questions: QuestionModel[] = [
     AnswerModel.wrongAnswer('Dó'),
     AnswerModel.rightAnswer('Lá'),
   ]),
-  new QuestionModel(
-    303,
-    'Qual símbolo indica o início da leitura musical na partitura?',
-    [
-      AnswerModel.wrongAnswer('Pausa'),
-      AnswerModel.wrongAnswer('Compasso'),
-      AnswerModel.wrongAnswer('Sustenido'),
-      AnswerModel.rightAnswer('Clave'),
-    ]
-  ),
-  new QuestionModel(304, 'Qual é o intervalo entre Dó e Fá?', [
-    AnswerModel.wrongAnswer('Segunda maior'),
-    AnswerModel.wrongAnswer('Terça menor'),
-    AnswerModel.wrongAnswer('Quinta justa'),
-    AnswerModel.rightAnswer('Quarta justa'),
-  ]),
-  new QuestionModel(305, 'Quantos sustenidos tem a escala de Ré maior?', [
-    AnswerModel.wrongAnswer('1 sustenido'),
-    AnswerModel.wrongAnswer('3 sustenidos'),
-    AnswerModel.wrongAnswer('4 sustenidos'),
-    AnswerModel.rightAnswer('2 sustenidos'),
-  ]),
-  new QuestionModel(306, 'Qual é o 6º grau no campo harmônico de Sol maior?', [
-    AnswerModel.wrongAnswer('Am'),
-    AnswerModel.wrongAnswer('Bm'),
-    AnswerModel.wrongAnswer('F#m'),
-    AnswerModel.rightAnswer('Em'),
-  ]),
-  new QuestionModel(
-    307,
-    'Qual desses acordes representa o modo menor natural?',
-    [
-      AnswerModel.wrongAnswer('C – D – E – F – G – A – B'),
-      AnswerModel.wrongAnswer('C – D – Eb – F – G – A – B'),
-      AnswerModel.wrongAnswer('C – D – Eb – F – G – Ab – B'),
-      AnswerModel.rightAnswer('C – D – Eb – F – G – Ab – Bb'),
-    ]
-  ),
-  new QuestionModel(308, 'Qual é o tom relativo menor de Sol maior?', [
-    AnswerModel.wrongAnswer('Dó menor'),
-    AnswerModel.wrongAnswer('Lá menor'),
-    AnswerModel.wrongAnswer('Si menor'),
-    AnswerModel.rightAnswer('Mi menor'),
-  ]),
-  new QuestionModel(309, 'Qual é a tríade que forma o acorde de Ré menor?', [
-    AnswerModel.wrongAnswer('D – F# – A'),
-    AnswerModel.wrongAnswer('D – G – A'),
-    AnswerModel.wrongAnswer('D – E – G'),
-    AnswerModel.rightAnswer('D – F – A'),
-  ]),
-  new QuestionModel(
-    310,
-    'Qual é o grau dominante no campo harmônico de Lá menor?',
-    [
-      AnswerModel.wrongAnswer('F'),
-      AnswerModel.wrongAnswer('E♭'),
-      AnswerModel.wrongAnswer('C'),
-      AnswerModel.rightAnswer('E'),
-    ]
-  ),
-  new QuestionModel(
-    311,
-    'Qual modo grego começa na segunda nota da escala maior?',
-    [
-      AnswerModel.wrongAnswer('Jônio'),
-      AnswerModel.wrongAnswer('Frígio'),
-      AnswerModel.wrongAnswer('Lídio'),
-      AnswerModel.rightAnswer('Dórico'),
-    ]
-  ),
-  new QuestionModel(312, 'Qual dessas escalas é considerada exótica?', [
-    AnswerModel.wrongAnswer('Escala maior'),
-    AnswerModel.wrongAnswer('Escala menor natural'),
-    AnswerModel.wrongAnswer('Escala dórica'),
-    AnswerModel.rightAnswer('Escala cigana'),
-  ]),
-  new QuestionModel(313, 'O que caracteriza um acorde diminuto?', [
-    AnswerModel.wrongAnswer('3ª maior e 5ª justa'),
-    AnswerModel.wrongAnswer('3ª menor e 5ª justa'),
-    AnswerModel.wrongAnswer('3ª maior e 5ª aumentada'),
-    AnswerModel.rightAnswer('3ª menor e 5ª diminuta'),
-  ]),
-  new QuestionModel(
-    314,
-    'Qual é a função harmônica de um acorde do 7º grau no campo harmônico maior?',
-    [
-      AnswerModel.wrongAnswer('Subdominante'),
-      AnswerModel.wrongAnswer('Tônica'),
-      AnswerModel.wrongAnswer('Dominante secundário'),
-      AnswerModel.rightAnswer('Função dominante'),
-    ]
-  ),
-  new QuestionModel(315, 'Qual é a sequência de graus do modo frígio?', [
-    AnswerModel.wrongAnswer('1 – 2 – 3 – 4 – 5 – 6 – 7'),
-    AnswerModel.wrongAnswer('1 – 2 – 3 – 4 – 5 – 6 – 7♭'),
-    AnswerModel.wrongAnswer('1 – 2 – 3♭ – 4 – 5♭ – 6 – 7'),
-    AnswerModel.rightAnswer('1 – 2♭ – 3♭ – 4 – 5 – 6♭ – 7♭'),
+  new QuestionModel(311, 'Quantos estados tem o Brasil?', [
+    AnswerModel.wrongAnswer('25'),
+    AnswerModel.wrongAnswer('28'),
+    AnswerModel.wrongAnswer('26 e 1 território'),
+    AnswerModel.rightAnswer('26 e o Distrito Federal'),
   ]),
 ]
 
